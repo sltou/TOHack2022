75a2ee2aecc070ef1b09659199610484630aebdd
stou-1 <sl.tou@mail.utoronto.ca>
2022-05-28 19:08:35 -0400

    add code to try to receive json data in flask

--- tryFlask.py
+++ tryFlask.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Create the receiver API POST endpoint:
+@app.route("/receiver", methods=["POST"])
+def postME():
+   data = request.get_json()
+   data = jsonify(data)
+   return data
