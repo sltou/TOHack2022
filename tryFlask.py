4c5b802d86c7801e9d0043f3015a9f8129408256
charlotte_volk <charlotte.volk@mail.mcgill.ca>
2022-05-28 21:50:16 -0400

    attempt webpage fix

--- tryFlask.py
+++ tryFlask.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#@app.route('/')
+#def index():
+   # return render_template('index.html')
 
 #Create the receiver API POST endpoint:
 @app.route("/receiver", methods=["POST"])
